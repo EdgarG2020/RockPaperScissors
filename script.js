f55e4a91ee87ecea23936bdd09e09d76ac03c29c
Edgar Guardado <guardadoedgar20@gmail.com>
2023-06-26 19:35:24 -0700

    Add a function that makes a random selection of ro ck, paper or scissors for the computer

--- script.js
+++ script.js
@@ -3,3 +3,23 @@ function getComputerChoice() {
     let choice = options[Math.floor(Math.random() * options.length)]
     return choice
 }
+
+function playRound(playerSelection, computerSelection) {
+    if(playerSelection === 'rock' && computerSelection === 'paper') {
+        return "you lose. paper beats rock."
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return "you win. paper beats rock"
+    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return "you win. rock beats scissors"
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        return 'you lose. rock beats scissors'
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return 'you win. scissors beats paper'
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        return 'you lose. scissors beats paper'
+    }
+}
+
+const playerSelection = ''
+const computerSelection = getComputerChoice()
+console.log(playRound(playerSelection, computerSelection))
