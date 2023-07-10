c31a1f5377b8d6e4361e9240c087ef33cec6c189
Edgar Guardado <guardadoedgar20@gmail.com>
2023-07-10 15:42:02 -0700

    Add prompt for user to make selection

--- script.js
+++ script.js
@@ -1,25 +1,35 @@
+playerScore = 0
+computerScore = 0
+
 function getComputerChoice() {
     let options = ['rock', 'paper', 'scissors']
     let choice = options[Math.floor(Math.random() * options.length)]
     return choice
 }
 
-function playRound(playerSelection, computerSelection) {
-    if(playerSelection === 'rock' && computerSelection === 'paper') {
-        return "you lose. paper beats rock."
-    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
-        return "you win. paper beats rock"
-    } else if (playerSelection === 'rock' && computerSelection === 'scissors') {
-        return "you win. rock beats scissors"
-    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
-        return 'you lose. rock beats scissors'
-    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
-        return 'you win. scissors beats paper'
-    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
-        return 'you lose. scissors beats paper'
+function playRound(playerSelection) {
+    let computerSelection = getComputerChoice()
+    let result = ''
+
+    if ((playerSelection == 'rock' && computerSelection == 'scissors') ||
+        (playerSelection == 'scissors' && computerSelection == 'paper') ||
+        (playerSelection == 'paper' && computerSelection == 'rock')) {
+
+        playerScore++
+        result = 'You win! ' + playerSelection + ' beats ' + computerSelection
+    } else if (playerSelection === computerSelection) {
+        result = 'Tie, you both chose ' + playerSelection
+    } else {
+        computerScore++
+        result = 'You lose. ' + computerSelection + ' beats ' + playerSelection
     }
+    return result
 }
 
-const playerSelection = ''
+const playerSelection = prompt('Rock, Paper, or Scissors?').toLowerCase()
 const computerSelection = getComputerChoice()
 console.log(playRound(playerSelection, computerSelection))
+
+
+
+
